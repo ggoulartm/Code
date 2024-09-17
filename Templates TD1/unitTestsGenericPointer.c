1fc66b7e5622465074e8b970254508d8839fb790
Gabriel Goulart <goulartg@MacBook-Pro-de-Gabriel.local>
2024-09-17 17:25:19 +0200

    Writing unit tests for generic pointer

---
+++ Templates TD1/unitTestsGenericPointer.c
@@ -0,0 +1,41 @@
+#include <time.h>
+#include "genericPointer.c"
+
+#define NBTESTS 100
+#define NBCAS 4
+// Programme de tests aleatoire
+
+
+int main() {
+  srand( time(NULL) );
+  table_t t1 = table_new(1000,NULL,NULL,NULL);
+  double *x;
+  int i,j,pos,nb;
+    x= malloc(sizeof(*x));
+  for (j=0; j<NBTESTS; j++) {
+    switch(random() % NBCAS) {
+      case 0: 
+      case 1:
+        x = random() % 100;
+        printf("Append de %.2lf \n",x);
+        t1=table_append(&x,t1);
+        break;
+      case 2:
+        if(t1->actual_size==0){break;}
+        x = random() % 100;
+        pos=random()%(t1->actual_size);
+        printf("Insertion de %.2lf en position %d\n",x,pos);
+        t1=table_insertat(&x,pos,t1);
+        break;
+      case 3:
+        if(t1->actual_size==0){break;}
+        pos=random()%(t1->actual_size);
+        printf("Suppression de l'element en position %d \n",pos);
+        t1=table_removeat(pos,t1);
+        break;
+    }
+    table_printf(t1);
+  }
+  printf("Liberation du tableau\n");
+  table_delete(t1);
+}
