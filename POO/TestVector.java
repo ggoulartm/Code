459ee3088e218d972c61daf21a059b0740b39690
goulartg <gabriel.goulart-muller@grenoble-inp.org>
2024-09-26 20:09:25 +0200

    TD2 - Vecteurs - complet

--- POO/TestVector.java
+++ POO/TestVector.java
@@ -1,7 +1,17 @@
 public class TestVector {
     public static void main(String[] args) {
-        Vector vR = new Vector(2);
-        vR.Insert(10,2); vR.Insert(3,4);
+        Vector vR = new Vector(3);
+        vR.Insert(1,2); vR.Insert(1,2); vR.Insert(1,3);
         System.out.println(vR);
+        System.out.println(vR.get(0));
+        vR.get(1).mult(vR.get(2));
+        System.out.println(vR.get(1));
+        System.out.println(vR.mult(2,3));
+        System.out.println(vR.add(2,3));
+        Vector vR2 = new Vector(2);
+        System.out.println(vR.add(vR2));
+        Vector vR3 = new Vector(3);
+        vR3.Insert(1,2); vR3.Insert(1,2); vR3.Insert(1,3);
+        System.out.println(vR.add(vR3));
     }
 }
--- POO/Vector.java
+++ POO/Vector.java
@@ -8,9 +8,10 @@ class Vector {
         this.R = new Rational[size];
     }
     public void Insert(int a, int b) {
-        Rational r = new Rational("v1",a,b);
+        Rational r = new Rational("v"+pos,a,b);
         this.Insert(r,pos);
         this.pos++;
+        this.size++;
     }
     public void Insert(Rational R, int pos) {
         if(pos >= size || pos < 0) { return; }
@@ -19,13 +20,46 @@ class Vector {
     public Rational[] Get() {
         return this.R;
     }
+    public int len() {
+        return this.R.length;
+    }
     public Rational get(int pos) {
         if(pos >= size || pos < 0) {
-            System.out.println("Index (%d) dehors de la taille du vecteur", pos);
-            return;
+            System.out.printf("Index (%d) dehors de la taille du vecteur\n", pos);
+            return R[0];
         }
         return this.R[pos];
     }
+    public Vector mult(int a, int b) {
+        System.out.println("Multiplier du vecteur tous les rationels par ("+a+"/"+b+")");
+        Rational r = new Rational("multiplier",a,b);
+        return this.mult(r);
+    }
+    public Vector mult(Rational r) {
+        for(Rational RationalUnit: this.R) {
+            RationalUnit.mult(r);
+        }
+        return this;
+    }
+    public Vector add(int a, int b) {
+        System.out.println("Ajouter du vecteur tous les rationels par ("+a+"/"+b+")");
+        Rational r = new Rational("adder",a,b);
+        return this.add(r);
+    }
+    public Vector add(Rational r) {
+        for(Rational RationalUnit: this.R) {
+            RationalUnit.add(r);
+        }
+        return this;
+    }
+    public Vector add(Vector v) {
+        if(v.len() != this.R.length) { System.err.println("Vecteur de longueur different"); return this;}
+        for(int i = 0; i < this.R.length; i++) {
+            this.R[i].add(v.get(i));
+        }
+        return this;
+    }
+
     public String toString() {
         this.str="";
         for(Rational r : this.R) {
